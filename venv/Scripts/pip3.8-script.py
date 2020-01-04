14a5efb9246e183e265c081896f154d521007673
Axxon <dominic.filion-guay@oculus.com>
2020-01-04 09:49:19 -0800

    Fixed up virtual environment.

--- venv/Scripts/pip3.8-script.py
+++ venv/Scripts/pip3.8-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\axxon\PycharmProjects\microblog\venv\Scripts\python.exe
+#!C:\Dev\microblog\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3.8'
 __requires__ = 'pip==19.0.3'
 import re
