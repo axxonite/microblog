06ff75f7fb2226b4b5075b6fe15bda09cc60d93a
Dominic Filion Guay <dominic.filion-guay@oculus.com>
2020-01-06 17:26:12 -0800

    Chapter 6: Profile Page and Avatars.

--- venv/Scripts/easy_install-script.py
+++ venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!C:\Dev\microblog\venv\Scripts\python.exe
+#!C:\Dev\Microblog\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
 __requires__ = 'setuptools==40.8.0'
 import re
